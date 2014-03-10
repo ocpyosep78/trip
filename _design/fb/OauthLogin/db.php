dc04deace209eab330ffd6b3ca7a1d964fb68054
her0satr <her0satr@yahoo.com>
2014-03-09 23:28:02 -0700

    update

---
+++ _design/fb/OauthLogin/db.php
@@ -0,0 +1,10 @@
+<?php
+$mysql_hostname = "localhost";
+$mysql_user = "suekare1_bromo";
+$mysql_password = "ErHpT-,e8roo";
+$mysql_database = "suekare1_bromo";
+$base_url='http://suekarea.com/';
+
+$bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
+mysql_select_db($mysql_database, $bd) or die("Could not select database");
+?>
