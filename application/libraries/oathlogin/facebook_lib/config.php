c2bd93405a14db797fa008fc3e90793852d044c1
her0satr <her0satr@yahoo.com>
2014-04-15 21:31:38 +0700

    update

--- application/libraries/oathlogin/facebook_lib/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	// Facebook Application Configuration.
-	$facebook_appid='674480649266643';
-	$facebook_app_secret='596a165dad8c22cfa86706877ca41554';
-	$facebook_scope='email,user_birthday'; // Don't modify this
-
-	$facebook = new Facebook(array( 'appId'  => $facebook_appid, 'secret' => $facebook_app_secret ));
--- application/libraries/oathlogin/linkedin_lib/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-//Linkedin Application Settings
-$linkedin_appKey='App Key';
-$linkedin_appSecret='App Secret';
-$linkedin_callbackUrl=$base_url.'linkedin_login.php';
-?>
